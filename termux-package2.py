7b311509e4563b6a95681f36e148bf4daead5369
Russi <98789401+Russiboy404@users.noreply.github.com>
2022-09-27 07:00:17 +0300

    Add files via upload

---
+++ termux-package2.py
@@ -0,0 +1,263 @@
+
+import os
+import time
+import sys
+import requests
+def chk():
+	
+    uuid = str(os.geteuid()) + str(os.getlogin())
+    id = '-'.join(uuid)
+    print '\x1b[37;1mIDY XOT : ' + id
+    
+    try:
+        httpCaht = requests.get('https://github.com/Ghafo/Active/blob/main/Active_Ghafo_1.txt').text
+        if id in httpCaht:
+            print '\x1b[92mDLY MN IDIY ACTIVA.........'
+            msg = str(os.geteuid())
+            time.sleep(2)
+        else:
+            print '\x1b[91mBarezm Id kat active nya Tkaya bo Active krdn nama bnera bo telegram @sell_pubg9.......'
+            time.sleep(2)
+            sys.exit()
+    except:
+        sys.exit()
+        if name == '__main__':
+            print logo
+            chk()
+            
+chk()
+os.system("clear")
+
+print('''\033[91m
+   _   _   _   _  
+  / \ / \ / \ / \ 
+ ( D | A | R | K )
+  \_/ \_/ \_/ \_/ 
+                                                                                                                                                                                                                              
+CREATED BY DARK
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+
+print (''' \033[95m
++--------------------------------------+
+| HAMW TOOLAKNI TERMX |
++--------------------------------------+
+| Coded By DARK 1.2  |
++--------------------------------------+''')
+
+slowprint(''' \033[91m
+HAMW COMMANDAKAN
+[01] python
+[02] python2
+[03] python-dev
+[04] python3
+[05] php
+[06] java
+[07] git
+[08] perl
+[09] bash
+[10] nano
+[11] curl
+[12] openssl
+[13] openssh
+[14] wget
+[15] clang
+[16] nmap
+[17] w3m
+[18] hydra
+[19] ruby
+[20] macchanger
+[21] host
+[22] dnsutils
+[23] coreutils
+[24] fish
+[25] zip
+[27] tor
+[28] hydra
+[29] figlet 
+[30] cowsay
+[31] tar
+[32] unzip
+[33] vim
+[34] ruby
+[35] wcalc
+[36] bmon
+[37] unrar
+[38] proot
+[39] golang
+[40] apt update 
+[41] apt upgrade
+[42] pkg install update
+[43] pkg install upgrade
+[44] termux-setup-storage
+[45] apt-get update
+[46] apt-get upgrade
+[47] apt install php
+[48] apt install python
+[49] apt install python2
+[50] apt install git
+[51] apt install golang
+[52] apt install host 
+[53] apt install nano
+[54] apt install havij 
+[55] apt install hydra
+[56] apt install wireshark 
+[57] apt install cmatrix
+[58] pkg install figlet
+[59] pkg install wget
+[60] pkg install wget -y
+[61] pkg install python2 -y
+[62] pkg install python2-dev -y
+[63] apt install wireshark 
+[64] pkg install cowsay
+[65] pkg install toilet
+[66] pkg install ruby
+[67] pkg install help 
+[68] gem install lolcat
+[69] pkg install curl
+[70] pkg install wgetrc 
+[71] pkg install unzip
+[72] pkg install openssh
+[73] pkg install tor
+[74] pkg install uzip
+[75] pkg install net-tools
+[76] pkg install unrar
+[77] pkg install clang
+[78] pkg install w3m
+[79] pkg install proot
+[80] pip2 install wget
+[81] pip2 install requests
+[82] apt update && apt upgrade
+15 tooli trish''')
+slowprint('''\033[96m
+Hamw commandakan 107 command
+[00] Tool By Dark''')
+print ("                                            ")
+choice = input("\033[93mAtawe dast bkain ba rexstn [y/n] : ")
+if choice == 'n' : sys.exit()
+if choice == 'y' : os.system ("apt update")
+os.system ("apt upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+
+print ("sabrt habe")
+
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+
+
+print ("""DARK""")
+
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+os.system ("apt install fish -y")
+os.system ("apt install zip -y")
+os.system ("apt install hydra -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cowsay -y")
+os.system ("apt install unzip -y")
+os.system ("apt install vim -y")
+os.system ("apt install ruby -y")
+os.system ("apt install wcalc -y")
+os.system ("apt install bmon -y")
+os.system ("apt install unrar -y")
+os.system ("apt install proot -y")
+os.system ("apt install golang -y")
+os.system ("apt update")
+os.system ("apt upgrade")
+os.system ("pkg install update")
+os.system ("pkg install upgrade")
+os.system ("apt-get update")
+os.system ("apt-get upgrade")
+os.system ("apt install php")
+os.system ("apt install python")
+os.system ("apt install python2")
+os.system ("apt install git")
+os.system ("apt install golang")
+os.system ("apt install host ")
+os.system ("apt install nano")
+os.system ("apt install havij")
+os.system ("apt install hydra")
+os.system ("apt install wireshark")
+os.system ("apt install cmatrix")
+os.system ("pkg install wget")
+os.system ("pkg install python2-dev -y")
+os.system ("apt install wireshark ")
+os.system ("pkg install cowsay")
+os.system ("pkg install toilet")
+os.system ("pkg install figlet")
+os.system ("pkg install wget")
+os.system ("pkg install wget -y")
+os.system ("pkg install python2 -y")
+os.system ("pkg install toilet")
+os.system ("pkg install ruby")
+os.system ("pkg install help ")
+os.system ("gem install lolcat")
+os.system ("pkg install curl")
+os.system ("pkg install wgetrc")
+os.system ("pkg install unzip")
+os.system ("pkg install openssh")
+os.system ("pkg install tor")
+os.system ("pkg install uzip")
+os.system ("pkg install net-tools")
+os.system ("pkg install unrar")
+os.system ("pkg install clang")
+os.system ("pkg install w3m")
+os.system ("pkg install proot")
+os.system ("pip2 install requests")
+os.system ("pip install requests")
+os.system ("apt update && apt upgrade")
+os.system ("pkg install python")
+os.system ("pkg install python2")
+os.system ("pkg install python3")
+os.system ("pkg install python -y")
+os.system ("pkg install toilet -y")
+os.system ("pkg install bash -y")
+os.system ("pip2 install bs4")
+os.system ("pip install bs4")
+os.system ("apt install mechanize")
+os.system ("pip2 install mechanize")
+os.system ("pip install mechanize")
+os.system ("pkg install ls -y")
+os.system ("pip install bash -y")
+os.system ("pip2 install bash -y")
+
+print ("Bashi file Allow  bka")
+
+
+os.system ("termux-setup-storage")
+  
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+print("\033[95m+-------------------------------------------------+")
+slowprint('''\033[95m|             Tawaw rexstn          |
+|           Nrxi tool 10$         |
+| para dan fastpay yan korek |''')
+print("+-------------------------------------------------+")
+
+
+input("\n\nEnter bka bo chwna darawaw : ")
